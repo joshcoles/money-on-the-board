7f9bdd130c7b33b77ddb8baa76eaa7cb1dfb656f
Josh Coles <josh@joshcoles.com>
2016-12-16 23:42:23 +0000

    Restructured file system. Tested logging in-game event descriptions and traversing JSON data.

--- server/server.js
+++ server/server.js
@@ -4,20 +4,20 @@ const PORT        = 3000;
 const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
-
+const game        = require('../mock-api/sample-data/sportsradar-play-by-play-sens-vs-leafs.json');
 
 app.set('view engine', 'ejs');
-// app.set('views', 'public/views');
+app.set('views', '../client/public/views');
 
+const pledge_events = ['goal', 'shotsaved', 'hit', 'penalty', 'assist']
 
 // app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
- res.render('index');
+  res.render('index', {game, pledge_events});
 });
-
 
 app.listen(PORT, () => {
  console.log('App Listening on Port ', PORT);
