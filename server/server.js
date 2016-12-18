4153cc3ec648963880e3f3339fd6cfe8595d06d4
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2016-12-18 20:52:22 +0000

    created form for creating a new campaign

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT                = 4000;
 const express             = require('express');
 const app                 = express();
 const bodyParser          = require('body-parser');
-const WebSocketServer     = require('ws');
+// const WebSocketServer     = require('ws');
 
 app.set('view engine', 'ejs');
 app.set('views', '../client/public/views');
@@ -36,7 +36,18 @@ app.get('/campaign/:id', (req, res) => {
 });
 
 app.post('/campaign/new', (req, res) => {
-  res.redirect("/campaign/:id")
+  console.log('form submitted')
+  let campaign_name = req.body.campaign_name
+  let charity_name = req.body.charity_name
+  let charity_url = req.body.charity_url
+  let hashtag = req.body.hashtag
+  let email = req.body.email
+  console.log(campaign_name)
+  console.log(charity_name)
+  console.log(charity_url)
+  console.log(hashtag)
+  console.log(email)
+  res.redirect("/campaign")
 });
 
 app.post('/campaign/:id/delete', (req, res) => {
