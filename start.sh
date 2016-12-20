45a47cf8c55eb928904eda04b74490684ec4fb2a
Adam <adam.dipinto@gmail.com>
2016-12-20 23:36:59 +0000

    included webpack -w to be run when all three servers are called upon.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd client/ ; npm start & cd ../mock-api/ ; npm run local & cd ../server/ ; npm run local
+cd client/ ; webpack -w & npm start & cd ../mock-api/ ; npm run local & cd ../server/ ; npm run local
