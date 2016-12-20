64ad793786565a6e9fac86c4dacab3bc75aad6bf
Adam <adam.dipinto@gmail.com>
2016-12-20 23:30:41 +0000

    Had to comment out previously constructed filtering function due to running into issues with web sockets and webpack.

--- client/src/Game.jsx
+++ client/src/Game.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import game from '../../mock-api/sample-data/sportsradar-play-by-play-sens-vs-leafs.json';
+// import game from '../../mock-api/sample-data/sportsradar-play-by-play-sens-vs-leafs.json';
 
 class Game extends Component {
 
@@ -14,27 +14,27 @@ class Game extends Component {
     const pledge_events = ['goal', 'shotsaved', 'hit', 'penalty', 'assist'];
     const pledge_events_array = [];
 
-    game.periods.forEach(function(period) {
-        console.log(period.sequence)
+    // game.periods.forEach(function(period) {
+    //     console.log(period.sequence)
 
-      period.events.forEach(function(event) {
-        if (pledge_events.includes(event.event_type)) {
-          pledge_events_array.push("Time " + event.clock + ": " + event.description);
-        }
-      })
-    })
+    //   period.events.forEach(function(event) {
+    //     if (pledge_events.includes(event.event_type)) {
+    //       pledge_events_array.push("Time " + event.clock + ": " + event.description);
+    //     }
+    //   })
+    // })
 
     let i = 0;
-    setInterval(function() {
-      if (i === pledge_events_array.length){
-        return
-      }
-      console.log(pledge_events_array[i])
-      i++
-    }, 2000);
+    // setInterval(function() {
+    //   if (i === pledge_events_array.length){
+    //     return
+    //   }
+    //   console.log(pledge_events_array[i])
+    //   i++
+    // }, 2000);
 
     this.setState({events: pledge_events_array});
-    
+
   }
 
 }
