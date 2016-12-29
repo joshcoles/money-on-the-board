af474603f243ea8689715c020b0edfcac3054c31
Adam <adam.dipinto@gmail.com>
2016-12-29 19:56:27 +0000

    Set isFlipped default setting to false rather than an open string.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isFlipped: '',
+      isFlipped: false,
       game: [],
       pledges: [],
       leaderboard: [
