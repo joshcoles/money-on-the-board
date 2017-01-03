adb0285061bbcca108421fdb66ceb04a9ed6a8d2
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-01-03 22:08:07 +0000

    modified campaigns fixtures to include a description field and image url field

--- server/fixtures/04_campaigns.js
+++ server/fixtures/04_campaigns.js
@@ -9,9 +9,11 @@ exports.seed = function(knex, Promise) {
           game_id: 1,
           user_id: 1,
           handle: '@SensMotB',
-          title: 'Eric - Senators vs Leafs',
+          title: 'Season Opener',
           charity_name: 'Senators Foundation',
           charity_url: 'www.donate.com',
+          description: 'Help raise funds and awareness for the Sens Foundation! Checkout @SensMotB on twitter for updates and share this campaign with friends and family.',
+          image_url: 'https://www.thestar.com/content/dam/thestar/opinion/editorials/2012/01/04/the_future_looks_bleak_for_canadian_minor_hockey/childhockey.jpeg'
         })
       ]);
     });
