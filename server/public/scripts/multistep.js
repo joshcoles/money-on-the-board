45e7646b7e78b29145a10ad96669b4b11bc00a96
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-01-10 19:40:09 +0000

    multistep form script placed in partial

--- server/public/scripts/multistep.js
+++ server/public/scripts/multistep.js
@@ -11,33 +11,49 @@ function _(x) {
 }
 function processPhase1() {
   game = _("game").value;
+  if(game) {
   _("phase1").style.display = "none";
   _("phase2").style.display = "block";
+  } else {
+    alert("Please select a game")
+  }
 }
 function processPhase2() {
   campaign_name = _("campaign_name").value;
   description = _("description").value;
+  if(campaign_name.length > 0 && description.length > 0) {
   _("phase2").style.display = "none";
   _("phase3").style.display = "block";
+  } else {
+    alert("Please provide a campaign name and description")
+  }
 }
 function processPhase3() {
   charity_name = _("charity_name").value;
   charity_url = _("charity_url").value;
+  if(charity_name.length > 0 && charity_url.length >0) {
   _("phase3").style.display = "none";
   _("phase4").style.display = "block";
+  } else {
+    alert("Please provide a charity name and donation link")
+  }
 }
 function processPhase4() {
   image_url = _("image_url").value;
   hashtag = _("hashtag").value;
+  if(image_url.length > 0 && hashtag.length > 0) {
   _("phase4").style.display = "none";
   _("show_all_data").style.display = "block";
-  _("display_game").innerHTML = game;
+  _("display_game").innerHTML = "Ottawa Senators vs. Toronto Maple Leafs";
   _("display_campaign_name").innerHTML = campaign_name;
   _("display_description").innerHTML = description;
   _("display_charity_name").innerHTML = charity_name;
   _("display_charity_url").innerHTML = charity_url;
   _("display_image_url").innerHTML = image_url;
   _("display_hashtag").innerHTML = hashtag;
+  } else {
+    alert("Please provide an image link and Twitter handle")
+  }
 }
 function submitForm() {
   _("multiphase").method = "post";
