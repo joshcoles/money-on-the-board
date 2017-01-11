204999bd08221e4fd86fb1d5d20550e8527d0e02
Adam <adam.dipinto@gmail.com>
2017-01-11 02:14:01 +0000

    Added new game uuid for calgary vs vancouver.

--- server/fixtures/01_games.js
+++ server/fixtures/01_games.js
@@ -8,6 +8,10 @@ exports.seed = function(knex, Promise) {
         knex('games').insert(
           {
             game_uuid: '4a130cc4-3bd4-42ad-ac93-484043a2d5bc'
+          }),
+        knex('games').insert(
+          {
+            game_uuid: '3b8e1099-8050-42c9-b545-2aa74e93b2e8'
           })
       ]);
     });
