5d34830a027a0bb20357943c2b3d782bcf4f3cca
Adam <adam.dipinto@gmail.com>
2017-02-13 20:45:27 +0000

    Created new migration for updated database. Created api call for players, teams, and schedule.

---
+++ server/api_call.js
@@ -0,0 +1,61 @@
+const request = require('request');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const db = require('./db');
+
+
+let teamUrl = 'http://statsapi.web.nhl.com/api/v1/teams';
+let teams = [];
+let players = [];
+let rosterUrl = [];
+
+request(teamUrl, (error, response, body) => {
+  if(!error && response.statusCode == 200) {
+    let teamListObj = JSON.parse(body);
+    let teamList = teamListObj.teams;
+    // console.log(teamList);
+    teamList.forEach((team) => {
+      let teamInfo = {
+        team_uuid: team.id,
+        team_fullname: team.name,
+        team_name: team.teamName,
+        team_abbreviation: team.abbreviation,
+        roster_url: 'https://statsapi.web.nhl.com/api/v1/teams/' + team.id + '/roster'
+      }
+      teams.push(teamInfo);
+      rosterUrl.push(teamInfo.roster_url);
+      db.insert([teamInfo])
+      .into('teams')
+      .then((result) => {
+        console.log("Team insterted", result);
+      });
+    });
+    // console.log(teams);
+    // console.log(rosterUrl);
+    rosterUrl.forEach((roster) => {
+      request(roster, (error, response, body) => {
+        if(!error && response.statusCode == 200) {
+          let rosterTeam = JSON.parse(body);
+          rosterTeam.roster.forEach((player) => {
+            let team_id = roster.slice(42, -7);
+            let playerInfo = {
+              player_uuid: player.person.id,
+              player_name: player.person.fullName,
+              player_jersey_number: player.jerseyNumber,
+              player_position: player.position.abbreviation,
+              team_id: team_id
+            }
+            players.push(playerInfo);
+            db.insert([playerInfo])
+            .into('players')
+            .then((result) => {
+              console.log("Players insterted", result);
+            });
+          });
+        };
+      });
+      // console.log(roster);
+    });
+  };
+});
